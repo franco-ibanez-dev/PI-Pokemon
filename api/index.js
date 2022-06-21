bdcf53adcf42651d359d93d28ab59932cc4e92e0
Fritz1996 <francoeibaez@gmail.com>
2022-06-21 17:12:44 -0300

    updated

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const { preChargeTypes } = require('./src/controllers/types.js');
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
+  preChargeTypes()
   console.log("Pokemon database connected")
   server.listen(3001, () => {
     console.log("Server listening at port 3001"); // eslint-disable-line no-console
--- api/src/controllers/types.js
+++ api/src/controllers/types.js
@@ -1,3 +1,31 @@
-const { Type } = require('../db.js')
-const { v4: uuidv4 } = require('uuid')
+const axios = require('axios');
+const { Type } = require('../db.js');
+const { v4: uuidv4 } = require('uuid');
+
+
+async function preChargeTypes(req, res, next) {
+    try {
+        const response = await axios('https://pokeapi.co/api/v2/type');
+        const responseArray = response.data.results;
+        const typesArray = [];
+        responseArray.forEach((elm) => typesArray.push(elm.name))
+        typesArray.forEach(async (elm) => {
+            await Type.findOrCreate({
+                where: {
+                    id: uuidv4(),
+                    name: elm,
+                }
+            })
+        })
+
+    } catch (err) {
+        next(err)
+    }
+}
+
+
+
+module.exports = {
+    preChargeTypes,
+}
 
