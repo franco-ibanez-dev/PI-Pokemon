114901652bba0e3d98a642babb78fcf80899991b
Fritz1996 <francoeibaez@gmail.com>
2022-07-04 15:36:27 -0300

    Updated

--- client/src/components/PokemonDetail/PokemonDetails.jsx
+++ client/src/components/PokemonDetail/PokemonDetails.jsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { getDetail } from '../../redux/actions/index.js';
-
+import style from './pokemonDetail.module.css'
 
 export function PokemonDetails(props) {
     // console.log(props);
@@ -21,24 +21,30 @@ export function PokemonDetails(props) {
         <div>
             {
                 myPokemon ?
-                    <div>
-                        <img src={myPokemon.sprite} alt="" width="250px" height="250px" />
-                        <h1>{myPokemon.name}</h1>
-                        <h3>Types: {myPokemon.types && myPokemon.types.map(elm => elm.name + ", ")}</h3>
-                        <h3>ID: {myPokemon.id}</h3>
-                        <ul> Stats:
+                    <div className={style.container}>
+                        <img
+                            id={style.sprite}
+                            src={myPokemon.sprite} alt="" width="250px" height="250px" />
+                        <h1 id={style.name}>{myPokemon.name}</h1>
+                        <h3 id={style.types}>Types: {myPokemon.types && myPokemon.types.map(elm => elm.name + ", ")}</h3>
+                        <h3 id={style.id}>ID: {myPokemon.id}</h3>
+                        <ul id={style.stats}> Stats:
                             <li>Life: {myPokemon.life}</li>
                             <li>Attack: {myPokemon.attack}</li>
                             <li>Defense: {myPokemon.defense}</li>
                             <li>Speed: {myPokemon.speed}</li>
                         </ul>
-                        <h3>Height: {myPokemon.height}</h3>
-                        <h3>Weight: {myPokemon.weight}</h3>
+                        <h3 id={style.height}>Height:  {myPokemon.height}</h3>
+                        <h3 id={style.weight}>Weight:    {myPokemon.weight}</h3>
                     </div> :
                     <p>Loading...</p>
             }
             <Link to="/home">
-                <button>Back home</button>
+                <button
+                    id={style.back}
+                    className={style.button}
+                >Back home
+                </button>
             </Link>
         </div>
     )
