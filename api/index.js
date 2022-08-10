3be2703f58b952bd516bdd5606d73c81cd4e8d15
Franco Ezequiel Ibañez <francoeibaez@gmail.com>
2022-08-10 12:59:00 -0300

    preparando el deploy del backend

--- api/index.js
+++ api/index.js
@@ -2,13 +2,13 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const { preChargeTypes } = require('./src/controllers/types.js');
-
+const { PORT } = process.env;
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   preChargeTypes()
   console.log("Pokemon database connected")
-  server.listen(3001, () => {
-    console.log("Server listening at port 3001"); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`Server listening at port ${PORT}`); // eslint-disable-line no-console
   });
 });
 
